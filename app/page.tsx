f00fe4da073e849550494e89e0c21225f0ca685d
Hatice Kaplan <4haticekaplan4@gmail.com>
2025-02-24 18:23:43 +0100

    add todoitem

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,94 @@
-import Image from "next/image";
+"use client";
+import Todos from '@/components/todos';
+import React, { useState, ChangeEvent } from 'react'
 
+type Todo = {
+  title: string;
+  desc: string;
+};
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+
+	const [todo, setTodo] = useState<Todo>({ title: "", desc: "" });
+	const addTodo = () => {
+		const todoString = localStorage.getItem("todos")
+		if (todoString) {
+			const todosJson : Todo[]= JSON.parse(todoString)
+			if (todosJson.filter((value) =>value.title == todo.title).length > 0) {
+				alert("Todo with this title already exists")
+			}
+			else {
+				todosJson.push(todo)
+				localStorage
+					.setItem("todos", JSON.stringify(todosJson))
+				alert("Todo has been added")
+				setTodo({title:"",desc:""});
+			}
+		}
+		else {
+			localStorage.setItem("todos", JSON.stringify([todo]));
+      alert("Todo has been added");
+      setTodo({title:"",desc:""});
+		}
+	}
+
+	const onChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+
+		setTodo(
+			{
+				...todo,
+				[e.target.name]: e.target.value
+			}
+		)
+		console.log(todo)
+	}
+	return (
+		<div className=" text-3xl">
+			<section className="text-gray-600 body-font">
+				<div className="container px-5 py-24 
+					mx-auto flex flex-wrap items-center">
+
+					<div className="rounded-lg p-8 flex flex-col 
+						md:ml-auto w-full mt-10 md:mt-0 bg-slate-100 ">
+						<h2 className="text-gray-900 text-lg 
+							font-medium title-font mb-5">
+							Add a Todo
+						</h2>
+						<div className="relative mb-4">
+							<label htmlFor="title"
+								className="leading-7 text-sm 
+									text-gray-600">
+								Todo Title
+							</label>
+							<input onChange={onChange} value={todo.title}
+								type="text" id="title" name="title"
+								className="w-full bg-white rounded border border-gray-300
+								focus:border-green-800 focus:ring-2 
+								focus:ring-green-200 text-base outline-none
+								text-gray-700 py-1 px-3
+							leading-8 transition-colors duration-200 ease-in-out"
+								autoComplete='false' />
+						</div>
+						<div className="relative mb-4">
+							<label htmlFor="desc" className="leading-7 text-sm text-gray-600">
+								Todo Description</label>
+							<input onChange={onChange} value={todo.desc}
+								type="text" id="desc" name="desc"
+								className="w-full bg-white rounded border 
+								border-gray-300 focus:border-green-800
+							focus:ring-2 focus:ring-green-200 text-base 
+							outline-none text-gray-700 py-1 px-3
+							leading-8 transition-colors duration-200 ease-in-out" autoComplete='false' />
+						</div>
+						<button onClick={addTodo} className="text-white 
+						bg-green-800 border-0 py-2 px-8
+						focus:outline-none w-fit hover:bg-green-600
+						rounded text-lg">Add Todo</button>
+
+					</div>
+				</div>
+			</section>
+      <Todos />
+		</div>
+	)
 }
